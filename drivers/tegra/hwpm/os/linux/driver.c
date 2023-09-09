c7c63cd0fe73c9dfbb904bac5fef745d4d2e8149
Shardar Mohammed <smohammed@nvidia.com>
2023-09-09 20:20:55 +0000

    hwpm: Remove module owner parameter
    Remove the module owner from the struct class based
    on following change in core kernel
    
    =====
        Upstream commit "6e30a66433af"
    
        driver core: class: remove struct module owner out of struct class
    
        The module owner field for a struct class was never actually used, so
        remove it as it is not doing anything at all.
    
        Cc: "Rafael J. Wysocki" <rafael@kernel.org>
        Link: https://lore.kernel.org/r/20230313181843.1207845-3-gregkh@linuxfoundation.org
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    =====
    
    Bug 4276500
    
    Change-Id: I0b68273e38f79ee6d903172b8f4d9d1807202abe
    Signed-off-by: Shardar Mohammed <smohammed@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-hwpm/+/2978633
    (cherry picked from commit f116216688239c2b67bbf3932fe68fb8c97b041e)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-hwpm/+/3015718
    Tested-by: Jonathan Hunter <jonathanh@nvidia.com>
    Reviewed-by: Vedashree Vidwans <vvidwans@nvidia.com>
    Reviewed-by: Jonathan Hunter <jonathanh@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>

--- drivers/tegra/hwpm/os/linux/driver.c
+++ drivers/tegra/hwpm/os/linux/driver.c
@@ -112,7 +112,9 @@ static int tegra_hwpm_probe(struct platform_device *pdev)
 	hwpm_linux->pdev = pdev;
 	hwpm_linux->dev = &pdev->dev;
 	hwpm_linux->np = pdev->dev.of_node;
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0))
 	hwpm_linux->class.owner = THIS_MODULE;
+#endif
 	hwpm_linux->class.name = TEGRA_SOC_HWPM_MODULE_NAME;
 
 	hwpm = &hwpm_linux->hwpm;
